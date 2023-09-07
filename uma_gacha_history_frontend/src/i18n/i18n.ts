19ece185949d64a00559a12ca01e3fa6bd83fa66
chinosk <2248589280@qq.com>
2023-09-07 23:46:55 +0800

    add tChinese by @yotv2000tw

--- uma_gacha_history_frontend/src/i18n/i18n.ts
+++ uma_gacha_history_frontend/src/i18n/i18n.ts
@@ -1,15 +1,20 @@
 import schinese from "../assets/i18n/schinese.json"
+import tchinese from "../assets/i18n/tchinese.json"
 import english from "../assets/i18n/english.json"
 
 export enum LanguageTypes {
-    SChinese,
-    English
+    SChinese = 0,
+    TChinese = 2,
+    English = 1,
 }
 
 export function i18nText(id: string, langId: LanguageTypes) {
     switch (langId) {
         case LanguageTypes.SChinese: {
             return schinese[id as keyof typeof schinese] || id;
+        }
+        case LanguageTypes.TChinese: {
+            return tchinese[id as keyof typeof tchinese] || id;
         }
         case LanguageTypes.English:
             return english[id as keyof typeof english] || id;
